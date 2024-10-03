f6214df924a035052e38a48058c8a287b9693ee8
carloseduardozx <carlospadilha66@gmail.com>
2024-10-03 15:36:38 -0300

    aula 03/10

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import (Flask, request)
 
 app = Flask(__name__)
 
 @app.route("/", methods=('GET',))
 def index():
-    nome = 'Carlos'
+    nome = request.args.get('Carlos')
     return f"""<h1>Página inicial</h1>
     <p>Olá {nome}, que nome bonito!
     """
