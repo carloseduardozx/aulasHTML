ed67cef40908b7d145aef0d72c0c768a4d154607
carloseduardozx <carlospadilha66@gmail.com>
2024-10-18 15:35:27 -0300

    Aula dia 18/10

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import (Flask, request)
+from flask import (Flask, render_template, request)
 app = Flask(__name__) # cria uma instância
 
 @app.route("/", methods=('GET',)) # Assina uma rota
@@ -56,9 +56,7 @@ def potencia(numero: float, elevado:float):
   return f""" 
 <h1> A área informada> n={numero}** e={elevado} Volume={numero**elevado} </h1>"""
 
-@app.route("/tabuada/<int:num>", methods=['GET'])
-def tabuada(num: int):   
-    html="<ul>"  
-    for i in range (1,11):
-      html+=f"<li> {num}x{i}={num*i}</li>"
-    return html + '</ul>'
+@app.route("/tabuada/<int:numero>", methods=['GET'])
+def tabuada(numero):
+
+  return render_template('tabuada.html', numero=numero)
